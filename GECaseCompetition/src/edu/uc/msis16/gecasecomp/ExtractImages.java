74e976317578f724df83b9d08541cc9a690a7d6b
kiranhsgithub <hskiran.bms@gmail.com>
2017-04-04 20:17:37 -0400

    pdf extract images

---
+++ GECaseCompetition/src/edu/uc/msis16/gecasecomp/ExtractImages.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package edu.uc.msis16.gecasecomp;
+
+
+
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Observer;
+
+import org.apache.pdfbox.tools.PDFToImage;
+
+public class ExtractImages {
+
+   static {
+      System.setProperty(
+         "org.apache.commons.logging.Log",
+         "org.apache.commons.logging.impl.NoOpLog" );
+   }
+
+   public static int extract( List< File > files, File jpegDir, Observer observer ) {
+      jpegDir.mkdirs();
+      int done = 0;
+      for( final File file : files ) {
+         try {
+            final File   target   = new File( jpegDir, file.getName());
+            final String trgtPath = target.getPath();
+            final String prefix   = trgtPath.substring( 0, trgtPath.lastIndexOf( '.' ));
+            PDFToImage.main( new String[]{ "-outputPrefix", prefix, file.getPath() });
+            final double percent  = (100.0 * ++done ) / files.size();
+            System.out.printf( "%6.2f %%: %s\n", percent, file.getName());
+            if( observer != null ) {
+               observer.update( null, file );
+            }
+         }
+         catch( final Throwable t ) {
+            System.err.println( file.getPath());
+            t.printStackTrace();
+         }
+      }
+      return done;
+   }
+
+   public static void main( String[] args ) {
+      
+      final File   pdfDir  = new File( "D:\\Google drive\\GE_Case_Comp\\searchable" );
+      final File   jpegDir = new File( "D:\\Google drive\\GE_Case_Comp\\images" );
+      final File[] files   = pdfDir.listFiles( new FilenameFilter() {
+         @Override public boolean accept( File dir, String name ) {
+            return name.toLowerCase().endsWith( ".pdf" );
+         }});
+      if( files != null ) {
+         final int done = extract( Arrays.asList( files ), jpegDir, null );
+         System.out.printf(
+            "\n%d file%s processed.", done, ( done > 1 ) ? "s" : "" );
+      }
+   }
+}
